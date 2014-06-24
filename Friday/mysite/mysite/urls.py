b431cd7d862da5e9e14782f74b4f953dba1e1547
Aurea Martinez <aurea@codeforprogress.org>
2014-06-24 12:34:21 -0400

    finished part 3git add -A

--- Friday/mysite/mysite/urls.py
+++ Friday/mysite/mysite/urls.py
@@ -5,6 +5,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
--- Friday/mysite/polls/views.py
+++ Friday/mysite/polls/views.py
@@ -1,7 +1,21 @@
-from django.http import HttpResponse
+from django. http import Http404
+from django.shortcuts import render, get_object_or_404
+
+from polls.models import Poll
 
 def index(request):
+	latest_poll_list = Poll.objects.order_by('-pub_date') [:5]
+	context = {'latest_poll_list': latest_poll_list}
+	return render(request, 'polls/index.html', context)
+	return HttpResponse(template.render(context))
 	return HttpResponse("Hello,world. You're at the poll index.")
 def detail(request, poll_id):
-	return HttpResponse("You're looking at poll  ")
+	poll = get_object_or_404(Poll, pk=poll_id)
+	return render(request, 'polls/detail.html', {'poll':poll})
+	return HttpResponse("You're looking at the results of poll %s." % poll_id)
+def results(request, poll_id):
+	return HttpResponse("You're looking at the results of poll %s." % poll_id)
+def vote(request, poll_id):
+	return HttpResponse("You're voting on poll %s." % poll_id)
+
 # Create your views here.
