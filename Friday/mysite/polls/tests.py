25e0edcec574934cbdbd9175b2288f61a61b029c
Aurea Martinez <aurea@codeforprogress.org>
2014-06-26 11:59:38 -0400

    starting to test

--- Friday/mysite/polls/tests.py
+++ Friday/mysite/polls/tests.py
@@ -1,3 +1,18 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
+
+from polls.models import Poll
+
+class PollMethodTests(TestCase):
+	"""
+	was_published_recently() should return False for polls whose
+	pub_date is in the future
+	"""
+
+	future_poll = Poll(pub_date=timezone.now() + datetime.timedelta(days=30))
+	self.assertEqual(future_poll.was_published_recently(),False)
+	
 
 # Create your tests here.
