46e3884cfc38e9d1a598fbef6e68e1fe8d87b01a
Aurea Martinez <aurea@codeforprogress.org>
2014-06-26 21:07:21 -0400

    I finished the tutorialgit add -Agit add -A

--- Friday/mysite/polls/views.py
+++ Friday/mysite/polls/views.py
@@ -2,26 +2,28 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
-
+from django.utils import timezone
 from polls.models import Choice, Poll
 
-def index(request):
-    latest_poll_list = Poll.objects.all().order_by('pub_date')[:5]
-    context = {'latest_poll_list': latest_poll_list}
-    return render(request, 'polls/index/html', context)
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_poll_list'
 
     def get_queryset(self):
-        """Return the last five published polls."""
-        return Poll.objects.order_by('-pub_date')[:5]
+        """Return the last five published polls (not including those set to be published in the future)."""
+        return Poll.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Poll
     template_name = 'polls/detail.html'
+
+def get_queryset(self):
+    """Excludes any polls that aren't published yet."""
+    return Poll.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
@@ -46,7 +48,3 @@ def vote(request, poll_id):
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
 
-    def results(request, poll_id):
-        poll = get_object_or_404(Poll, pk=poll_id)
-
-        return render(request, 'polls/results.html', {'poll': poll})
